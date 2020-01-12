c7dab137dad94b20c888eaa9fac4a86a446f8180
Gerard Cruz <gerardbcruz@gmail.com>
2020-01-12 01:51:48 -0500

    Finished Search Functionality script

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,9 @@ lightbox.option({
      'wrapAround': true,
      'showImageNumberLabel': false,
      'fadeDuration': 600
-   })
+})
 
-   // First try to get the value of the search field whenever a user types in that field, and log
-   // that value to the screen with a console.log() statement. Checkout the keyup event
-   // listener for this. And be sure to make the value case insensitive by using something like
-   // the toLowerCase() method.
-   // Next, start trying to target the entire caption by using the getAttribute() method, then
-   // looping and logging the all lowercase version of the captions to the console.
-   // Next comes the fun part. Start trying to find ways to check if the current value of the
-   // search input is included within any of the captions, and if so, log the associated image
-   // to the console.
-   // Lastly, use a conditional so that if there's a match, display the container of the image,
-   // and if not, hide it. Piece of cake!
-
+//Search Functionality
 const $search = $('#search');
 const $links = $('a');
 
@@ -27,10 +16,10 @@ $search.keyup(function() {
   //loop through a link tags
   $links.each(function() {
     let $captions = $(this).attr('data-title').toLowerCase();
-    if () {
-      $links.hide();
-      $(this).show();
+    if ($captions.indexOf($searchValue) > -1) {
+      $(this).parent().show();
+    } else {
+      $(this).parent().hide();
     }
   })
-
 })
